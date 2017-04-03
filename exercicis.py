96a65392f6e6187085ca44124b5a339d49baa1fd
Brandon <Brandonqr1@gmail.com>
2017-04-03 20:10:15 +0200

    nuevo ejercicio agregado

--- exercicis.py
+++ exercicis.py
@@ -1,5 +1,12 @@
 from random import randint
+import datetime
 def exercici1():
     print "Hello Moto"
 def exercici2():
     print "Numero: ",randint(0,1000)
+def exercici3():
+    dataActual=datetime.datetime.now()
+    horaFormatada = str(dataActual.hour)+":"+str(dataActual.minute)+":"+str(dataActual.second)
+    archi=open('datos.txt','w')
+    archi.write(horaFormatada)
+    archi.close()
--- menu.py
+++ menu.py
@@ -5,14 +5,17 @@ def main_menu():
   os.system('clear')
   print "1.--------------"
   print "2.Random Classic"
-  print "3.Salir"
+  print "3.Reloj"
+  print "4.Salir"
   print "-------------"
   opcio=raw_input(">>")
   if opcio=='1':
     exercici1()
   elif opcio=='2':
     exercici2()
-  elif opcio='3':
+  elif opcio=='3':
+    exercici3()
+  elif opcio='4':
     return
   else:
     print "Error."
